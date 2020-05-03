892fe370c20b7b2d21fd9a32dc44311816f897b3
Eric Moynihan <git@moynihan.io>
2020-05-03 22:23:41 +0100

    Added entypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,14 @@ endpoint() {
     curl --max-time 30 --no-buffer -s --unix-socket /var/run/docker.sock "$@" || return 1
     return 0
 }
-
 while true; do
     sleep 5
-    endpoint "http://localhost/containers/json?filters=\{\"health\":\[\"unhealthy\"\]${labelFilter}\}"
+    endpoint "http://localhost/containers/json?filters=\{\"health\":\[\"unhealthy\"\]\}" | \
+    jq -r "foreach .[] as \$CONTAINER([];[]; \$CONTAINER | .Id, .Names[0] )" | \
+    while read -r CONTAINER_ID && read -r CONTAINER_NAME; do
+        if [ "null" != "$CONTAINER_NAME" ] && [ -f "/scripts$CONTAINER_NAME.sh" ]; then
+            echo "Unhealthy container found. Running script for $CONTAINER_NAME"
+            /scripts$CONTAINER_NAME.sh
+        fi
+    done
 done
