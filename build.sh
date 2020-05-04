29ddc9cc5b63cd88cb32b58c19a9a25fc1b7fbe2
Eric Moynihan <git@moynihan.io>
2020-05-04 15:39:19 +0100

    Added build fix

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-REPO=docker.pkg.github.com/ericm/healscript/healscript
+REPO=moynihan/healscript
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 
 echo "Building x86_64"
-docker build --build-arg -t ${REPO}:latest -t ${REPO}:x86_64 .
+docker build -t ${REPO}:latest -t ${REPO}:x86_64 .
 docker push ${REPO}:latest
 docker push ${REPO}:x86_64
 
